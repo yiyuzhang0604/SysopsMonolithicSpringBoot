d0f66f441358079bd0cf0c223a586b083756ef74
Ying <zy414371666@gmail.com>
2023-03-23 15:20:24 -0700

    add Param annotation to input parameter

--- src/main/java/com/sysops/dao/ArticleDao.java
+++ src/main/java/com/sysops/dao/ArticleDao.java
@@ -3,10 +3,11 @@ package com.sysops.dao;
 import com.sysops.entity.Article;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArticleDao extends CrudRepository<Article, Long> {
     @Query("SELECT article FROM Article a WHERE a.title LIKE %:keyword% OR a.content LIKE %:keyword%")
-    List<Article> findByKeyword(String keyword);
+    List<Article> findByKeyword(@Param("keyword") String keyword);
 }
